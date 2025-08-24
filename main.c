8d1688113001fd23b166739fe885f038f4413e9b
Benjamin Bruno Meier <benjamin.meier70@gmail.com>
2025-08-24 20:24:44 +0200

    New example

--- main.c
+++ main.c
@@ -13,12 +13,11 @@ void print_int_list(const List(int) *l)
 int main()
 {
     List(int) l = {};
-    ListElement(int) *l0 = list_append(&l, 10);
-    ListElement(int) *l1 = list_prepend(&l, -12);
-    printf("l0->value=%d\n", l0->value);
-    printf("l1->value=%d\n", l1->value);
+    list_append(&l, 10);
+    ListElement(int) *el = list_prepend(&l, -12);
+    printf("el->value=%d\n", el->value);
     print_int_list(&l);
-    list_remove(&l, l0);
+    list_remove(&l, el);
     print_int_list(&l);
     list_clear(&l);
 }
